fb33c6721a7d62cda82c3f2c02590c0aafcb2dd4
rakhmeto <abrambest2008@yandex.ru>
2023-03-10 20:27:20 +0600

    start nc

---
+++ main.go
@@ -0,0 +1,138 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"net"
+	"os"
+	"strings"
+	"sync"
+)
+
+type StructUsers struct {
+	Addr net.Conn
+	Name string
+}
+
+var (
+	usersMap = make(map[string]StructUsers)
+	mu       sync.Mutex
+)
+
+func checkNames(name string) bool {
+	for names := range usersMap {
+		if name == names {
+			return false
+		}
+	}
+	return true
+}
+
+func checkNamesFonts(name string) bool {
+	for _, s := range name {
+		if (s < '0' || s > '9') && (s < 'A' || s > 'Z') && (s < 'a' || s > 'z') {
+			fmt.Println("false")
+			return false
+		}
+	}
+	fmt.Println("true")
+	return true
+}
+
+func handle(clientConn net.Conn) {
+	defer clientConn.Close()
+	var getNameUser string
+
+	welcome, err := os.ReadFile("welcome.txt")
+	if err != nil {
+		log.Println(err)
+	}
+	clientConn.Write(welcome)
+
+	for {
+		clientConn.Write([]byte("[ENTER YOUR NAME]: "))
+
+		name := bufio.NewReader(clientConn)
+		getNameUser, err = name.ReadString('\n')
+		if err != nil {
+			log.Println(err)
+			return
+		}
+
+		if len(usersMap) > 9 {
+			clientConn.Write([]byte("Chat is full, try later...\n"))
+			continue
+		} else if !checkNames(strings.TrimSpace(getNameUser)) {
+			clientConn.Write([]byte("Name is busy\n"))
+			continue
+		} else if !checkNamesFonts(strings.TrimSpace(getNameUser)) {
+			clientConn.Write([]byte("Incorrect username\n"))
+			continue
+		} else if strings.TrimSpace(getNameUser) == "\n" {
+			continue
+		} else {
+			break
+		}
+	}
+
+	userName := strings.TrimSpace(getNameUser)
+
+	user := StructUsers{
+		Addr: clientConn,
+		Name: userName,
+	}
+
+	usersMap[user.Name] = user
+
+	// fmt.Printf("Name user: %s, RemoteAddr: %s\n", userName, clientConn.RemoteAddr())
+
+	for {
+		b := bufio.NewReader(clientConn)
+		line, err := b.ReadBytes('\n')
+		if err != nil {
+			log.Println(err, user.Name)
+			return
+		}
+
+	}
+}
+
+func main() {
+	port := ""
+	if len(os.Args) > 2 {
+		log.Println("[USAGE]: ./TCPChat $port")
+		return
+	}
+	if len(os.Args) == 2 {
+		port = os.Args[1]
+	} else {
+		port = "8989"
+	}
+
+	fmt.Println("Listening on the port :" + port)
+
+	server, err := net.Listen("tcp", ":"+port)
+	if err != nil {
+		log.Println(err, "-> Please input port: \"Example: go run . 8080\"")
+		return
+	}
+	defer server.Close()
+
+	history, err := os.Create("history.txt")
+	if err != nil {
+		log.Println(err)
+	}
+	defer history.Close()
+
+	for {
+
+		client, err := server.Accept()
+		if err != nil {
+			log.Println(err.Error())
+		}
+
+		go handle(client)
+
+	}
+}
